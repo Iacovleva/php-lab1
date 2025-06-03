977aa7d9b1f25e0a6484b5b2d7ea02f3cd676c96
Iacovleva <lwwc.yyy@gmail.com>
2025-06-03 23:09:24 +0300

    Лабораторная работа №1: первая программа на PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Привет, мир!";
+
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+
+
+// Создание переменных
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+// Вывод с использованием конкатенации
+echo $message . " Через " . $days . " дней!<br>";
+
+// Вывод с использованием двойных кавычек
+echo "$message Через $days дней!<br>";
+?>
